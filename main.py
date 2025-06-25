64c4cb0a972152bed3a2877b80508f2ec4c78b1f
Matthew Whelehan <matthew.whelehan@gmail.com>
2025-06-25 16:32:05 -0400

    added new learnings

--- main.py
+++ main.py
@@ -10,3 +10,32 @@ if weather.strip().lower() == "hot":
 
 else:
     print(f"Oh its {weather} today the temp is {Temperature}")
+
+#Numeric Input + Type Conversion
+age = "What is your age?"
+# this is a string not a number 
+age = int(input("how old are you"))
+#int makes it a number now
+age = int(input("what is your age?"))
+if age >= 18:
+    print("You are adult")
+else:
+    print("You are a kid")
+
+#Mini Project: Neon Café Greeter
+
+name = input("Welcome, what is you name?").strip
+print(f"Nice to meat you {name}")
+weather = input("whats the weather like today").strip().lower()
+mood = input ("How are you doing today?").strip().lower()
+print(f"yes when the weather is {weather} it makes you feel {mood})")
+
+if weather == "very hot":
+    print("can I offer you some iced tea")
+elif weather == "warm":
+    print("Can I offer you some iced tea")
+elif weather == "chilly":
+    print("Can I offer you some hot tea")
+else:
+    print (f"because weather is {weather} what would like today")
+
